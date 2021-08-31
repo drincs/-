2cd9b9c31f9223203dc418d03f7ba316bf273371
drincs <drincs@github>
2021-08-31 19:25:05 +0800

    update 0831

--- PAT-Advanced/练习/1141 PAT Ranking of Institutions (25 分)/1141.cpp
+++ PAT-Advanced/练习/1141 PAT Ranking of Institutions (25 分)/1141.cpp
@@ -3,64 +3,81 @@
 #include <math.h>
 #include <algorithm>
 #include <unordered_map>
+#include <string.h>
+#include <string>
+#include <iomanip>
 using namespace std;
-struct school{
+struct school
+{
     string id;
-    double tws=0;
-    int ns=0;
+    double tws = 0;
+    int ns = 0;
     int rank;
-    school(string id){
-        this->id=id;
+    school(string id)
+    {
+        this->id = id;
     }
 };
 vector<school> all;
-unordered_map<string,int> getId;
-bool cmp(school &a,school &b){
-    if(a.tws==b.tws){
-        if(a.ns==b.ns){
-            return a.id<b.id;
+unordered_map<string, int> getId;
+bool cmp(school &a, school &b)
+{
+    if (a.tws == b.tws)
+    {
+        if (a.ns == b.ns)
+        {
+            return a.id < b.id;
         }
-        return a.ns<b.ns;
+        return a.ns < b.ns;
     }
-    return a.tws>b.tws;
+    return a.tws > b.tws;
 }
-int main(){
+int main()
+{
     int n;
-    int count=0;
-    for(int i=0;i<n;i++){
-        string sid,id;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        string sid, id;
         int score;
-        cin>>sid>>score>>id;
-        id=tolower(id);
-        if(getId.count(id)==0){
-            getId.insert(make_pair(id,count));
+        cin >> sid >> score >> id;
+        id = tolower(id);
+        if (getId.count(id) == 0)
+        {
+            getId.insert(make_pair(id, count));
             all.push_back(school(id));
             count++;
         }
-        int iid=getId[id];
-        if(sid[0]=='A'){
-            all[iid].tws+=score;
-        }else if(sid[0]=='B'){
-            all[iid].tws+=score*1.0/1.5;
-        }else{
-            all[iid].tws+=score*1.5;
+        int iid = getId[id];
+        if (sid[0] == 'A')
+        {
+            all[iid].tws += score;
+        }
+        else if (sid[0] == 'B')
+        {
+            all[iid].tws += score * 1.0 / 1.5;
+        }
+        else
+        {
+            all[iid].tws += score * 1.5;
         }
         all[iid].ns++;
     }
-    sort(all.begin(),all.end(),cmp);
-    int rank=1;
-    double now=all[0].tws;
-    count<<count<<endl;
-    for(int i=0;i<count;){
-        int j=i;
-        while(now==all[j].tws){
-            cout<<rank<<" "<<all[j].id<<" "<<all[i].tws<<" "<<all[i].ns<<endl;  
+    sort(all.begin(), all.end(), cmp);
+    int rank = 1;
+    double now = all[0].tws;
+    count << count << endl;
+    for (int i = 0; i < count;)
+    {
+        int j = i;
+        while (now == all[j].tws)
+        {
+            cout << rank << " " << all[j].id << " " << all[i].tws << " " << all[i].ns << endl;
             j++;
         }
-        now=all[j].tws;
-        i=j;
+        now = all[j].tws;
+        i = j;
     }
     system("pause");
     return 0;
-
 }
--- PAT-Advanced/练习/1143 Lowest Common Ancestor (30 分)/1143.cpp
+++ PAT-Advanced/练习/1143 Lowest Common Ancestor (30 分)/1143.cpp
@@ -1,19 +1,69 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <set>
 using namespace std;
-
-vector<int> tree;
-int main(){
-    int m,n;
-    scanf("%d%d",&m,&n);
-    for(int i=0;i<n;i++){
-        int key;
-        scanf("%d",&key);
-        tree.push_back(key);
+struct node
+{
+    node *lchild = NULL;
+    node *rchild = NULL;
+    int key;
+    node(int key)
+    {
+        this->key = key;
     }
-    while(m--){
-        int u,v;
-        scanf("%d%d",&u,&v);
+};
+unordered_map<int, int> nums;
+node *root = NULL;
+void insert(node *root, int key)
+{
+    if (root == NULL)
+    {
+        root = new node(key);
+    }
+    else
+    {
+        if (root->key > key)
+        {
+            insert(root->lchild, key);
+        }
+        else
+        {
+            insert(root->rchild, key);
+        }
+    }
+}
+int main()
+{
+    int m, n;
+    scanf("%d%d", &m, &n);
+    for (int i = 0; i < n; i++)
+    {
+        int key;
+        scanf("%d", &key);
+        insert(root, key);
+        nums.insert(make_pair(key, 1));
+    }
+    while (m--)
+    {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        if (nums.count(u) == 0 || nums.count(v) == 0)
+        {
+            if (nums.count(u) == 0 && nums.count(v) == 0)
+            {
+                printf("ERROR: %d and %d are not found.\n", u, v);
+            }
+            else if (nums.count(u) == 0)
+            {
+                printf("ERROR: %d is not found.\n", u);
+            }else{
+                printf("ERROR: %d is not found.\n", v);
+            }
+        }else{
+            
+        }
     }
     system("pause");
     return 0;
